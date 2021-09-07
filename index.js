481ae87b6a517eb06ab64eece52c77e575f8c6df
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-09-07 12:15:56 -0300

    Ejercicio 8

--- index.js
+++ index.js
@@ -80,21 +80,55 @@ console.log(
 );
 //true*/
 
-// 5 Definí una función generarMatrizEscalonada que reciba como argumento un número entero filas y devuelva un array 2d con la cantidad de filas filas, donde la primera fila tiene 1 columna, la segunda tiene 2, la tercera 3, y así sucesivamente. Los ítems de la matriz deben ser 0.
+//4 Definí una función generarGrilla que tome por parámetro un número entero filas, un número entero columnas y un array de valores items, y devuelva una matriz de filas filas y columnas columnas, donde cada ítem de la matriz es un ítem aleatorio de items.
+/*const obtenerNumeroAlAzar = (items) => {
+  return Math.floor(Math.random() * items.length);
+};
+
+const generarGrilla = (filas, columnas, items) => {
+  let matriz = [];
+
+  for (let i = 0; i < filas; i++) {
+    let arrayFilas = [];
+    matriz.push(arrayFilas);
+
+    for (let j = 0; j < columnas; j++) {
+      arrayFilas.push(items[obtenerNumeroAlAzar(items)]);
+    }
+  }
+
+  return matriz;
+};
+
+console.log(generarGrilla(2, 3, [1, 2]));
+//[
+//  [1, 1, 2],
+//  [2, 1, 1]
+//]
+console.log(generarGrilla(3, 3, ["a", "b", "c"]));
+//[
+//['c', 'c', 'a'],
+//['c', 'a', 'a'],
+//['b', 'a', 'b']
+//]*/
+
+//5 Definí una función generarMatrizEscalonada que reciba como argumento un número entero filas y devuelva un array 2d con la cantidad de filas filas, donde la primera fila tiene 1 columna, la segunda tiene 2, la tercera 3, y así sucesivamente. Los ítems de la matriz deben ser 0.
+
 // NO FUNCIONA
-
 /*const generarMatrizEscalonada = (numero) => {
-  let acc = [];
+  let matriz = [];
 
-  for (let i = 0; i < numero; i++) {
-    acc.push([]);
+  for (let i = 0; i < 1; i++) {
+    let acc = "";
+    let array = [];
+    matriz.push(array);
+    for (let j = 0; j < numero; j++) {
+      acc = acc + "0";
+      array.push(acc);
+    }
   }
 
-  for (let j = 0; j < acc.length; j++) {
-   
-  }
-
-  return acc;
+  return matriz;
 };*/
 //console.log(generarMatrizEscalonada(3));
 /*[
@@ -102,14 +136,10 @@ console.log(
    [0, 0], 
    [0, 0, 0]
  ] */
-/*console.log(generarMatrizEscalonada(5)) 
- [
-   [0], 
-   [0, 0], 
-   [0, 0, 0], 
-   [0, 0, 0, 0], 
-   [0, 0, 0, 0, 0]
- ] */
+//console.log(generarMatrizEscalonada(5));
+/*[
+  ([0], [0, 0], [0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0, 0])
+];*/
 
 //6 Definí una función obtenerFilaMasLarga que reciba como argumento un array 2d matriz y devuelva el array (fila) que tenga mayor longitud. Si hay varios de igual longitud, debe devolver el primero de ellos.
 
@@ -156,3 +186,92 @@ console.log(
   ])
 );
 //4*/
+//8 Definí una función convertirEnMatriz que reciba como argumentos un número entero columnas y un array array y devuelva una matriz con los items de array y la cantidad de columnas columnas por fila. La última fila puede quedarse como menos columnas de las indicadas si no hay más ítems que agregar.
+
+/*const convertirEnMatriz = (columnas, array) =>{
+  let matriz = []
+for (let i = 0; i < array.length; i++) {
+  let arrayCortado = array.splice(0,columnas)
+  matriz.push(arrayCortado)
+}
+  return matriz
+
+}
+ console.log(convertirEnMatriz(2, [1, 2, 3, 4]))
+//[[1, 2], [3, 4]]
+ console.log(convertirEnMatriz(3, [1, 2, 3, 4, 5, 6, 7]))
+//[[1, 2, 3], [4, 5, 6], [7]]
+ console.log(convertirEnMatriz(4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+//[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11]]*/
+
+
+//10 Definí una función tieneBloqueHorizontal que reciba como argumento un array 2d matriz y devuelva si dicha matriz tiene un bloque horizontal de 3 o más ítems consecutivos idénticos.
+
+/*const tieneBloqueHorizontal = (array) => {
+  let resultado = false;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] === array[i][j + 1] && array[i][j] === array[i][j + 2]) {
+        resultado = true;
+        return resultado;
+      } else {
+        resultado = false;
+      }
+    }
+  }
+  return resultado;
+};
+
+console.log(
+  tieneBloqueHorizontal([
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ])
+);
+//false
+
+console.log(
+  tieneBloqueHorizontal([
+    [1, 2, 3, 4],
+    [1, 2, 2, 2],
+    [1, 2, 4, 5],
+  ])
+);
+//true*/
+
+//11 Definí una función tieneBloqueVertical que tome una matriz y devuelva si la misma tiene un bloque vertical  de 3 o más ítems consecutivos idénticos .
+
+/*const tieneBloqueVertical = (array) => {
+  let resultado = false;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] === array[i + 1][j] && array[i][j] === array[i + 2][j]) {
+        resultado = true;
+        return resultado;
+      } else {
+        resultado = false;
+      }
+    }
+  }
+  return resultado;
+};
+
+console.log(
+  tieneBloqueVertical([
+    [4, 2, 3],
+    [1, 5, 3],
+    [1, 2, 3],
+  ])
+);
+//true
+
+console.log(
+  tieneBloqueVertical([
+    [1, 2, 3, 4],
+    [7, 2, 9, 2],
+    [1, 8, 4, 5],
+  ])
+);
+//false*/
